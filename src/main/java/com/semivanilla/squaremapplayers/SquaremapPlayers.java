fad66152914be922526dc52e3906e585bdec77e7
Alen Alex <alengeoalex@gmail.com>
2022-02-05 23:08:41 +0530

    test-push

--- src/main/java/com/semivanilla/squaremapplayers/SquaremapPlayers.java
+++ src/main/java/com/semivanilla/squaremapplayers/SquaremapPlayers.java
@@ -7,6 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SquaremapPlayers extends JavaPlugin {
 
+
     private static SquaremapPlayers instance;
     private SquaremapHook squaremapHook;
 
